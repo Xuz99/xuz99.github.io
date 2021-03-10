4738c45122eec319de41db7ad545622d0f099c1a
John J Davidson <xuz99@live.co.uk>
2021-03-10 17:32:25 +0000

    Moved Instagram feed to custom.js

--- custom.js
+++ custom.js
@@ -1,3 +1,20 @@
+// Template literal for my instagram feed
+
+var feed = new Instafeed({
+  accessToken: InstagramToken,
+  limit: 12,
+  template: `<figure class="photo shadow-lg bg-black text-white overflow-hidden rounded-lg">
+            <a class="cursor-pointer" href="{{link}}" target="_blank">
+                <img src="{{image}}" alt="" class="w-full h-60 object-cover hover:opacity-75 ease-in duration-200">
+            </a>
+            <figcaption class="m-4 leading-7">
+                <span class="block">{{caption}}</span>
+            </figcaption>
+            </figure>    
+            `,
+});
+feed.run();
+
 // auto date on footer
 const date = new Date();
 const year = date.getFullYear();
