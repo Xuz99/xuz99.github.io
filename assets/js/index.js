56438f2468dd9d509d8914633b0ff12954e4a645
John J Davidson <xuz99@live.co.uk>
2022-06-26 11:21:26 +0100

    Removed borken ig feed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,18 @@
-// Template literal for my instagram feed
-var feed = new Instafeed({
-  accessToken: InstagramToken,
-  limit: 12,
-  template: `<figure class="photo shadow-lg bg-black text-white overflow-hidden rounded-lg">
-            <a class="cursor-pointer" href="{{link}}" target="_blank" title="{{caption}}" rel="noopener">
-                <img src="{{image}}" alt="" class="w-full h-80 object-cover hover:opacity-75 ease-in duration-200">
-            </a>
-            <figcaption class="m-4  overflow-auto h-24 leading-7">
-                <span class="block">{{caption}}</span>
-            </figcaption>
-            </figure>    
-            `,
-});
-feed.run();
+// // Template literal for my instagram feed
+// var feed = new Instafeed({
+//   accessToken: InstagramToken,
+//   limit: 12,
+//   template: `<figure class="photo shadow-lg bg-black text-white overflow-hidden rounded-lg">
+//             <a class="cursor-pointer" href="{{link}}" target="_blank" title="{{caption}}" rel="noopener">
+//                 <img src="{{image}}" alt="" class="w-full h-80 object-cover hover:opacity-75 ease-in duration-200">
+//             </a>
+//             <figcaption class="m-4  overflow-auto h-24 leading-7">
+//                 <span class="block">{{caption}}</span>
+//             </figcaption>
+//             </figure>    
+//             `,
+// });
+// feed.run();
 
 // auto date on footer
 const date = new Date();
